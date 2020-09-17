7d4e8fc69b78758c983666b256ecd7850739799e
Nusiba Ibrahim <ainusiba@gmail.com>
2020-09-17 09:06:28 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,1 +1,11 @@
 // create your App component here
+
+ componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          peopleInSpace: data.people
+        })
+      })
+  }
